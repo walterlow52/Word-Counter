b75eee6314cc918ce795cd4b557efc5a332ab1df
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-05-11 16:07:05 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+var target = document.querySelector("wordcount");
+var wordCount = document.querySelector("wordCount");
+var charCount = document.querySelector("characterCount");
+var count = function() {
+  var chars = countTarget.value;
+  var char_length = chars.length;
+  var words = characters.split(/[nrs]+/g).filter(function(word) {
+    return word.length > 0; 
+  });
+  wordCount.innerHTML = words.length;
+  charCount.innerHTML = char_length;
+};
+count();
+window.addEventListener("input", function(event) {
+  if (event.target.matches("#wordcount")) {
+    count();
+  } else {
+    return false; 
+  }
+});
