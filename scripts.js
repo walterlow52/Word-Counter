2fb2d5a438f0bf9d7037d2127f1589723b1e0f2b
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-06-06 19:33:51 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
-var target = document.querySelector("#wordcount");
-var wordCount = document.querySelector("#wordCount");
-var charCount = document.querySelector("#characterCount");
 var count = function() {
-  var chars = target.value;
+  var chars = document.getElementById("word_input").value;
   var char_length = chars.length;
-  var words = characters.split(/[nrs]+/g).filter(function(word) {
+  var words = chars.split(/[nrs]+/g).filter(function(word) {
     return word.length > 0; 
   });
-  wordCount.innerHTML = words.length;
-  charCount.innerHTML = char_length;
+  document.getElementById("wordCount").innerHTML = words.length;
+  document.getElementById("characterCount").innerHTML = char_length;
 };
+
 count();
+
 window.addEventListener("input", function(event) {
-  if (event.target.matches("#wordcount")) {
+  if (event.target.matches("#word_input")) {
     count();
   } else {
     return false; 
